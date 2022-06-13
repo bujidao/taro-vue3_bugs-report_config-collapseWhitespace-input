6359287a6566f77c1450a20c9e0e52dfb4bd24cc
Alex ceng <izengjing@163.com>
2022-06-13 11:51:37 +0800

    init

---
+++ src/pages/index/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <view class="index">
+    <text>{{ msg }}</text>
+    <input type="text" style="background: #aaa;" placeholder="请输入关键字">
+  </view>
+</template>
+
+<script>
+import { ref } from 'vue'
+import './index.scss'
+
+export default {
+  name: 'homePage',
+  setup () {
+    const msg = ref('Hello world')
+    return {
+      msg
+    }
+  }
+}
+</script>
